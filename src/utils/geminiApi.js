04f71c36a7b3eaf52ccb1b0644d99b1264c1acbf
Ayush <ayuofficial11310@gmail.com>
2025-08-02 02:36:38 +0530

    Fixed Small Issue

--- src/utils/geminiApi.js
+++ src/utils/geminiApi.js
@@ -3,7 +3,7 @@ import { GEMINI_API_KEY } from "./constants";
 
 // The client gets the API key from the environment variable `GEMINI_API_KEY`.
 const genai = new GoogleGenAI({
-    apiKey: GEMINI_API_KEY
+    apiKey: process.env.REACT_APP_GEMINI_API_KEY,
 });
 
 
